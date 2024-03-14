b101a002e57df03bdb8b69b3ba47b6fd4d568614
jo3l4 <joelchavez789@yahoo.com>
2024-03-13 23:56:37 -0700

    addedToTheCode

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const PORT = 8080;
 
 // GET - / - returns homepage
 app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/public/index.html`);
     // serve up the public folder as static index.html file
 
 });
@@ -20,6 +21,7 @@ app.get('/api', (req, res) => {
 
 // get all pets from the database
 app.get('/api/v1/pets', (req, res) => {
+    res.json(pets);
     // send the pets array as a response
 
 });
@@ -27,26 +29,28 @@ app.get('/api/v1/pets', (req, res) => {
 // get pet by owner with query string
 app.get('/api/v1/pets/owner', (req, res) => {
     // get the owner from the request
-
+    const owner = req.query.owner;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.owner === owner);
 
     // send the pet as a response
-
+    res.json(pet);
 });
+
 
 // get pet by name
 app.get('/api/v1/pets/:name', (req, res) => {
     // get the name from the request
-
+    const name = req.params.name;
 
     // find the pet in the pets array
     const pet = pets.find(pet => pet.name === name);
 
     // send the pet as a response
-
+    res.json(pet);
 });
+
 
 app.listen(PORT, () => {
     console.log('Server is listening on port ' + PORT);
